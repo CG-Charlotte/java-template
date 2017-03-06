aa1adf1fa15ee58b339faf10fd3c46256cd10f59
charlotte <charlotte@codingame.com>
2017-03-06 15:52:53 +0100

    files sutructure

--- java-project/src/test/java/com/yourself/ExampleTest.java
+++ java-project/src/test/java/com/yourself/ExampleTest.java
@@ -4,6 +4,8 @@ package com.yourself;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.IOException;
+
 public class ExampleTest{
 
     @Test
@@ -13,7 +15,8 @@ public class ExampleTest{
     }
 
     @Test
-    public void visualisation(){
+    public void visualisation() throws IOException {
+        Process p = Runtime.getRuntime().exec("ls /project/target ");
         System.out.println("CG> open -s /project/target/target/classes HelloWorld.html");
         System.out.println("CG> open -s /project/target HelloWorld.html");
 
