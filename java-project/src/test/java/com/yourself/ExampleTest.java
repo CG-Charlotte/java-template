2fb074f0036ad75c2b979eaceb1a1673a877b550
charlotte <charlotte@codingame.com>
2017-03-06 16:07:31 +0100

    files sutructure

--- java-project/src/test/java/com/yourself/ExampleTest.java
+++ java-project/src/test/java/com/yourself/ExampleTest.java
@@ -18,7 +18,7 @@ public class ExampleTest{
 
     @Test
     public void visualisation() throws IOException {
-        Process p = Runtime.getRuntime().exec("ls /project/workspace/ ");
+        Process p = Runtime.getRuntime().exec("ls /project/target/ ");
 
         BufferedReader stdInput = new BufferedReader(new
                 InputStreamReader(p.getInputStream()));
